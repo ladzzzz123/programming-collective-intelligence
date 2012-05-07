8ccc49ccf202f93425b2636ad6fa2cec18cd7009
Owain Lewis <owain@owainlewis.com>
2012-05-07 13:03:24 +0100

    chapter 2 work

--- src/pci/chapter02.clj
+++ src/pci/chapter02.clj
@@ -1,24 +1,31 @@
 (ns pci.chapter02
+  (:use clojure.set)
   (:require pci.data))
 
-(defn results-for-critic
+(defn results-for
   "Return the scores for a specific critic"
   [critic-name]
   (get critics critic-name))
 
+(defn key-intersection
+  "Given two maps, returns the common keys as a Clojure set"
+  [m1 m2]
+  (let [a (set (keys m1))
+        b (set (keys m2))]
+    (intersection a b)))
+  
 ;; Finding similar users. Euclidean distance and Pearson correlation
 
 ;; Returns a distance-based similarity score for person1 and person2
 
-(defn square [x] (* x x))
-
 (defn distance
   "Take the distance in each axis, square them and add them together"
   [x y]
-  (let [[a b] x
-        [c d] y]
-  (Math/sqrt
-    (+ (square (- a c)) (square (- b d)))))) 
+  (letfn [(square [x] (* x x))]
+    (let [[a b] x
+          [c d] y]
+    (Math/sqrt
+      (+ (square (- a c)) (square (- b d))))))) 
 
 (defn distance-normalized
   "A function that gives higher values for people that are similar"
@@ -26,6 +33,12 @@
   (/ 1 (+ 1 (distance x y))))
 
 (defn sim-distance
-  "Return a distance based similarity score for person 1 and person 2"
+  ""
   [prefs person1 person2]
-  ())
+  (let [p1 (prefs person1)
+        p2 (prefs person2)
+        ;; get the list of shared_items
+        si (key-intersection p1 p2)]
+    (if (zero? (count si)) ;; return 0 if no shared items
+      0
+      ())))   
